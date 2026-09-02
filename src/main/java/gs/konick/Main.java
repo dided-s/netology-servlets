bc73e47a2dac5f4b49ddd5052ead2fb628589955
gilsar <konick.qlippoth@gmail.com>
2026-09-02 18:50:19 +0300

    Migration

--- src/main/java/gs/konick/Main.java
+++ src/main/java/gs/konick/Main.java
@@ -1,9 +1,32 @@
 package gs.konick;
 
-import gs.konick.service.PostService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class Main {
+
+    public static void main(String[] args) throws LifecycleException, IOException {
+        final var tomcat = new Tomcat();
+        final var baseDir = Files.createTempDirectory("tomcat");
+        baseDir.toFile().deleteOnExit();
+        tomcat.setBaseDir(baseDir.toAbsolutePath().toString());
+
+        final var connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        tomcat.getHost().setAppBase(".");
+        tomcat.addWebapp("", ".");
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+    /*
     public static void main(String[] args) {
         // отдаём список пакетов, в которых нужно искать аннотированные классы
         final var context = new AnnotationConfigApplicationContext("gs.konick");
@@ -17,4 +40,5 @@ public class Main {
         // по умолчанию создаётся лишь один объект на BeanDefinition
         final var isSame = service == context.getBean("postService");
     }
+     */
 }
